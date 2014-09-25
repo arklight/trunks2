97a0b37f6b1361303ee6cd1bfb52d792ce23d798
arklight <prohobbs@gmail.com>
2014-09-25 12:08:48 -0400

    bfreeman fix trunks repo

--- ex11_a.py
+++
@@ -1,7 +0,0 @@
-print "How old do you want to be?",
-age = raw_input('--->')
-print "how many cars do you want to own?",
-cars = raw_input('--->')
-print "You want to be %r old and have %r cars!!" % (
-    age, cars
-)
--- ex13a.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-
-# unpacks argv
-script, first_name, last_name = argv
-
-# string replacement
-print "Your first name and last name: %s, %s " % (
-    first_name, last_name
-)
